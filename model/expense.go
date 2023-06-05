ca129b82c9e74749813dc860aad76a2b221b632c
PeemXD <error074xd@gmail.com>
2023-06-05 16:33:12 +0700

    ++

--- model/expense.go
+++ model/expense.go
@@ -2,10 +2,11 @@ package model
 
 import "github.com/lib/pq"
 
+//! ,omitempty in json, if not have field or fill null and if struct type string, it will be set to ""
 type Expenses struct {
 	ID     uint           `gorm:"primaryKey column:id" json:"id"`
-	Title  string         `gorm:"column:title" json:"title"`
-	Amount float32        `gorm:"column:amount" json:"amount"`
+	Title  string         `gorm:"column:title" json:"title,omitempty"`
+	Amount float32        `gorm:"column:amount" json:"amount,omitempty"`
 	Note   string         `gorm:"column:note" json:"note"`
 	Tags   pq.StringArray `gorm:"type:text[];column:tags" json:"tags"`
 }
